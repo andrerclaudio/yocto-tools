56b64fc9a20954d243090ca29a89d48a727ce3ef
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2023-08-26 14:43:13 -0300

    Added file in C to initialze the Oled display.

---
+++ meta-utils/recipes-drivers/oled/kernel-oled_0.1.bb
@@ -0,0 +1,17 @@
+SUMMARY = "Make an I2C oled display workable."
+DESCRIPTION = "${SUMMARY}"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+inherit module
+
+SRC_URI = "file://Makefile \
+           file://kernel-oled-driver.c \
+          "
+
+S = "${WORKDIR}"
+
+# The inherit of module.bbclass will automatically name module packages with
+# "kernel-module-" prefix as required by the oe-core build environment.
+
+RPROVIDES:${PN} += "kernel-module-kdriveroled"
--- scripts/imx8mn-smbus/imx8mn_ssd1306.py
+++ scripts/imx8mn-smbus/imx8mn_ssd1306.py
@@ -22,13 +22,12 @@
 # THE SOFTWARE.
 from __future__ import division
 import logging
-import time
 from smbus import SMBus
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # Constants
-SSD1306_I2C_ADDRESS = 0x3C    # 011110+SA0+RW - 0x3C or 0x3D
+SSD1306_I2C_ADDRESS = 0x3C  # 011110+SA0+RW - 0x3C or 0x3D
 SSD1306_SETCONTRAST = 0x81
 SSD1306_DISPLAYALLON_RESUME = 0xA4
 SSD1306_DISPLAYALLON = 0xA5
@@ -64,6 +63,7 @@ SSD1306_LEFT_HORIZONTAL_SCROLL = 0x27
 SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29
 SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A
 
+
 class SSD1306_128_32(object):
 
     def __init__(self, i2c_bus, i2c_address=SSD1306_I2C_ADDRESS, vccstate=SSD1306_SWITCHCAPVCC):
@@ -71,83 +71,83 @@ class SSD1306_128_32(object):
         self._log = logging.getLogger('Diplay Oled SSD1306 implementation!')
         self.width = 128
         self.height = 32
-        self._pages = self.height//8
-        self._buffer = [0]*(self.width*self._pages)
+        self._pages = self.height // 8
+        self._buffer = [0] * (self.width * self._pages)
         # Save vcc state.
         self._vccstate = vccstate
         # Handle hardware I2C
         self._i2c_address = i2c_address
         self._i2c = SMBus(i2c_bus)
-        logging.info(self._i2c)      
-        self._initialize()  
+        logging.info(self._i2c)
+        self._initialize()
 
     def _initialize(self):
         # 128x32 pixel specific initialization.
-        self.command(SSD1306_DISPLAYOFF)                    # 0xAE
-        self.command(SSD1306_SETDISPLAYCLOCKDIV)            # 0xD5
-        self.command(0x80)                                  # the suggested ratio 0x80
-        self.command(SSD1306_SETMULTIPLEX)                  # 0xA8
+        self.command(SSD1306_DISPLAYOFF)  # 0xAE
+        self.command(SSD1306_SETDISPLAYCLOCKDIV)  # 0xD5
+        self.command(0x80)  # the suggested ratio 0x80
+        self.command(SSD1306_SETMULTIPLEX)  # 0xA8
         self.command(0x1F)
-        self.command(SSD1306_SETDISPLAYOFFSET)              # 0xD3
-        self.command(0x0)                                   # no offset
-        self.command(SSD1306_SETSTARTLINE | 0x0)            # line #0
-        self.command(SSD1306_CHARGEPUMP)                    # 0x8D
+        self.command(SSD1306_SETDISPLAYOFFSET)  # 0xD3
+        self.command(0x0)  # no offset
+        self.command(SSD1306_SETSTARTLINE | 0x0)  # line #0
+        self.command(SSD1306_CHARGEPUMP)  # 0x8D
         if self._vccstate == SSD1306_EXTERNALVCC:
             self.command(0x10)
         else:
             self.command(0x14)
-        self.command(SSD1306_MEMORYMODE)                    # 0x20
-        self.command(0x00)                                  # 0x0 act like ks0108
+        self.command(SSD1306_MEMORYMODE)  # 0x20
+        self.command(0x00)  # 0x0 act like ks0108
         self.command(SSD1306_SEGREMAP | 0x1)
         self.command(SSD1306_COMSCANDEC)
-        self.command(SSD1306_SETCOMPINS)                    # 0xDA
+        self.command(SSD1306_SETCOMPINS)  # 0xDA
         self.command(0x02)
-        self.command(SSD1306_SETCONTRAST)                   # 0x81
+        self.command(SSD1306_SETCONTRAST)  # 0x81
         self.command(0x8F)
-        self.command(SSD1306_SETPRECHARGE)                  # 0xd9
+        self.command(SSD1306_SETPRECHARGE)  # 0xd9
         if self._vccstate == SSD1306_EXTERNALVCC:
             self.command(0x22)
         else:
             self.command(0xF1)
-        self.command(SSD1306_SETVCOMDETECT)                 # 0xDB
+        self.command(SSD1306_SETVCOMDETECT)  # 0xDB
         self.command(0x40)
-        self.command(SSD1306_DISPLAYALLON_RESUME)           # 0xA4
-        self.command(SSD1306_NORMALDISPLAY)                 # 0xA6
-    
+        self.command(SSD1306_DISPLAYALLON_RESUME)  # 0xA4
+        self.command(SSD1306_NORMALDISPLAY)  # 0xA6
+
     def close(self):
         """Turn off the Display and freed the bus."""
-        self.command(SSD1306_DISPLAYOFF)                    # 0xAE
+        self.command(SSD1306_DISPLAYOFF)  # 0xAE
         self._i2c.close()
 
     def command(self, c):
-        """Send command byte to display."""    
+        """Send command byte to display."""
         # I2C write.
-        control = 0x00   # Co = 0, DC = 0
+        control = 0x00  # Co = 0, DC = 0
         self._i2c.write_byte_data(self._i2c_address, control, c)
 
     def data(self, c):
-        """Send byte of data to display."""        
+        """Send byte of data to display."""
         # I2C write.
-        control = 0x40   # Co = 0, DC = 0
+        control = 0x40  # Co = 0, DC = 0
         self._i2c.write_byte_data(self._i2c_address, control, c)
 
     def begin(self):
-        """Initialize display."""        
+        """Initialize display."""
         # Turn on the display.
         self.command(SSD1306_DISPLAYON)
 
     def display(self):
         """Write display buffer to physical display."""
         self.command(SSD1306_COLUMNADDR)
-        self.command(0)              # Column start address. (0 = reset)
-        self.command(self.width-1)   # Column end address.
+        self.command(0)  # Column start address. (0 = reset)
+        self.command(self.width - 1)  # Column end address.
         self.command(SSD1306_PAGEADDR)
-        self.command(0)              # Page start address. (0 = reset)
-        self.command(self._pages-1)  # Page end address.
+        self.command(0)  # Page start address. (0 = reset)
+        self.command(self._pages - 1)  # Page end address.
         # Write buffer data.        
-        for i in range(0, len(self._buffer), 16): 
-            control = 0x40   # Co = 0, DC = 0           
-            self._i2c.write_i2c_block_data(self._i2c_address, control, self._buffer[i:i+16])
+        for i in range(0, len(self._buffer), 16):
+            control = 0x40  # Co = 0, DC = 0
+            self._i2c.write_i2c_block_data(self._i2c_address, control, self._buffer[i:i + 16])
 
     def image(self, image):
         """Set buffer to value of Python Imaging Library image. The image should
@@ -155,30 +155,29 @@ class SSD1306_128_32(object):
         """
         if image.mode != '1':
             raise ValueError('Image must be in mode 1.')
-        imwidth, imheight = image.size
-        if imwidth != self.width or imheight != self.height:
-            raise ValueError('Image must be same dimensions as display ({0}x{1}).' \
-                .format(self.width, self.height))
-        # Grab all the pixels from the image, faster than getpixel.
+        width, height = image.size
+        if width != self.width or height != self.height:
+            raise ValueError('Image must be same dimensions as display ({0}x{1})'.format(self.width, self.height))
+        # Grab all the pixels from the image, faster than get pixel.
         pix = image.load()
         # Iterate through the memory pages
         index = 0
         for page in range(self._pages):
-            # Iterate through all x axis columns.
+            # Iterate through all x-axis columns.
             for x in range(self.width):
                 # Set the bits for the column of pixels at the current position.
                 bits = 0
                 # Don't use range here as it's a bit slow
                 for bit in [0, 1, 2, 3, 4, 5, 6, 7]:
                     bits = bits << 1
-                    bits |= 0 if pix[(x, page*8+7-bit)] == 0 else 1
+                    bits |= 0 if pix[(x, page * 8 + 7 - bit)] == 0 else 1
                 # Update buffer byte and increment to next byte.
                 self._buffer[index] = bits
                 index += 1
 
     def clear(self):
         """Clear contents of image buffer."""
-        self._buffer = [0]*(self.width*self._pages)
+        self._buffer = [0] * (self.width * self._pages)
 
     def set_contrast(self, contrast):
         """Sets the contrast of the display.  Contrast should be a value between
@@ -201,4 +200,3 @@ class SSD1306_128_32(object):
             else:
                 contrast = 0xCF
             self.set_contrast(contrast)
-  
